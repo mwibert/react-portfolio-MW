8e4b84434512b107cef2c2206c2c5e585ce60cbf
Mike Wibert <michaelwibert@gmail.com>
2025-04-14 01:24:44 -0600

    removing contact page until it is functional

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,7 +19,7 @@ function Navigation() {
       {[
         { label: "About Me", path: "/about" },
         { label: "Portfolio", path: "/portfolio" },
-        { label: "Contact", path: "/contact" },
+        // { label: "Contact", path: "/contact" },
         { label: "Resume", path: "/resume" },
       ].map((item, index) => (
         <NavLink
